0bea27019b66c9e7df43ffdde8bb6317e8c40cd4
Sabahat001 <sabahats504@gmail.com>
2024-03-26 20:42:12 +0500

    login form updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState } from 'react';
-
+import "./App.css";
 
 function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  // const [rememberMe, setRememberMe] = useState(false);
 
+  // const handleCheckboxChange = () => {
+  //   setRememberMe(!rememberMe);
+  // };
+
+  
   const handleLogin = (e) => {
     console.log('Username:', username);
     console.log('Password:', password);
@@ -14,8 +20,12 @@ function App() {
   };
 
   return (
-    <div className="login-container">
-      <form className="login-form" onSubmit={handleLogin}>
+    <>
+    <h1 className='heading'>Log In</h1>
+    <div class="container">
+
+    <div class="login-form">
+      <form  onSubmit={handleLogin}>
         <input
           type="text"
           placeholder="Username"
@@ -26,13 +36,30 @@ function App() {
         <input
           type="password"
           placeholder="Password"
-          value={password}
+          value={password} 
           onChange={(e) => setPassword(e.target.value)}
           required
         />
+         {/* <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={rememberMe}
+              onChange={handleCheckboxChange}
+            />Remember Me
+          </label>
+        </div> */}
         <button type="submit">Login</button>
       </form>
-    </div>
+
+   </div>
+  <div class="image">
+    <img src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=" alt="Img" />
+  </div>
+</div>
+
+
+</>
   );
 }
 
