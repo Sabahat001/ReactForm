659557a45dd32152bafc9e77a7064a12bf447791
Sabahat001 <sabahats504@gmail.com>
2024-03-29 22:47:33 +0500

    login form images updated

--- src/App.js
+++ src/App.js
@@ -23,11 +23,14 @@ function App() {
   return (
     <>
       <div className="logo-container">
-        <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9wppjcCkilEavui6RyxtUsYFLkWNorWwGsQ&s" alt="" /></div>
+        <img className="logo" src="https://cdn.discordapp.com/attachments/1215365922814955520/1223324390943363104/Group_408.png?ex=6619709f&is=6606fb9f&hm=07fa33f74dee3f66bd550e41aa78af2079ae3d2f9881a04c38bc0e4484f44fc2&" alt="" /></div>
       <h1 className='heading'>Log In</h1>
+      
       <div className="social-media">
-        <div className="google"><i class="fa-brands fa-google"></i> Google </div>
-        <div className="fb"><i class="fa-brands fa-facebook-f"></i>  Facebook</div>
+        <div className="google"> 
+        <img src="https://cdn.discordapp.com/attachments/1215365922814955520/1223324391161594020/Logo.png?ex=6619709f&is=6606fb9f&hm=4d0b1fa0ae35b78063267650dc3cb1482c2831efecfd0909ae8f25bf7e851e15&" alt="" />  Google
+        </div>
+        <div className="fb"><img src="https://cdn.discordapp.com/attachments/1215365922814955520/1223324391379566756/logo_1.png?ex=6619709f&is=6606fb9f&hm=e05268c92eb4c5ecd1ce1a11d826e222d03a7d39e2c4366e49299564fd5c3123&" alt="" /> Facebook</div>
       </div>
 
       <div className="horizontal-line"></div>
@@ -71,7 +74,7 @@ function App() {
         </div>
 
         <div class="image">
-          <img src="https://media.istockphoto.com/id/1281150061/vector/register-account-submit-access-login-password-username-internet-online-website-concept.jpg?s=612x612&w=0&k=20&c=9HWSuA9IaU4o-CK6fALBS5eaO1ubnsM08EOYwgbwGBo=" alt="Img" />
+          <img src="https://cdn.discordapp.com/attachments/1215365922814955520/1223324390683312260/Illustration.png?ex=6619709f&is=6606fb9f&hm=d07719dc92676a498eebf908443efe074cbc8806b96d715af7de57060c22aaed&" alt="Img" />
         </div>
 
       </div>
